76e563933764bfa5af10368e656f80a9e121fa5a
Will Parker <will.parker0@gmail.com>
2016-09-18 20:32:55 +0000

    plot_ball1 is done except for Travis nosetest

--- plot_ball1.py
+++ plot_ball1.py
@@ -11,22 +11,38 @@ on the same figure. """
 def f(t, v):
     return (v*t - (1.0/2.0)*9.81*(t**2))
 
-
-
 def getVTY():
-"""Takes user input for v, creates arrays for t and the resulting f(t,v)."""
-    v = [int(i) for i in raw_input("Enter your list of v_0's, separated by commas: ").split(",")]
-    np.asarray(v)
-    t = np.linspace(0, v/9.81, 40)
-    vf = np.vectorize(f)
-    y = vf(t, v)
+    """Takes user input for v, and creates arrays for t and the resulting 
+    f(t,v). So for each i, v[i], t[i], y[i] will be an initial value,
+    an array of 100 times, and an array of 100 data points (y values).
+    This function returns a list l = [v, t, y]."""
+    #creates an array for v based on user input, split at the comma
+    v = np.array([float(i) for i in raw_input("Enter your list of v_0's, separated by commas: ").split(",")])
+    
+    #creates an array of arrays, where each elemental array is a set of t's
+    #corresponding to the set of y's for the SAME v_0
+    t= np.array([np.linspace(0, float(i)/9.81, 100) for i in v])
 
-def plot1(t, y, v):
-    plt.plot(t, y)
+    #creates array of arrays, where each elemental array is defined like:
+    #y[i]=vf(t[i], v)
+    vf = np.vectorize(f)
+    y = np.asarray([vf(t[i], v[i]) for i in range(len(v))])
+    l = [v, t, y]
+    return l
+
+print getVTY()
+
+def plot1(v, t, y):
+    """This function takes input in the form of v=array, t=array of arrays,
+    y=array of arrays such that for each entry in v, t and y have a set of 
+    data points. It will return a plot of those data points for each v_0,
+    each plot on the same figure."""
+    for i in xrange(len(v)):
+        plt.plot(t[i], y[i])
+        plt.hold('on')
     plt.xlabel('t')
-    plt.ylabel('h')
-    plt.axis([0.0, v/9.81, 0.0, np.amax(y)+3])
-    plt.savefig('y(t).pdf')
+    plt.ylabel('y')
+    plt.title('Height vs time')
     plt.show()
 
 
