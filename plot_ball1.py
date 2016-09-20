7983ddea3d4686f5b8fe7316b4f96c19755eb0fe
Will Parker <will.parker0@gmail.com>
2016-09-20 08:02:44 +0000

    Final Draft(probably)

--- plot_ball1.py
+++ plot_ball1.py
@@ -11,15 +11,18 @@ on the same figure. """
 def f(t, v):
     return (v*t - (1.0/2.0)*9.81*(t**2))
 
-def getVTY():
-    """Takes user input for v, and creates arrays for t and the resulting 
-    f(t,v). So for each i, v[i], t[i], y[i] will be an initial value,
-    an array of 100 times, and an array of 100 data points (y values).
-    This function returns a list l = [v, t, y]."""
-    #creates an array for v based on user input, split at the comma
-    
+def getV():
+    """Takes user input for a set of v_0's, returns them as array, for
+    use later in getTY()"""
     v = np.array([float(i) for i in raw_input("Enter your list of v_0's, separated by commas: ").split(",")])
+    return v
     
+def getTY(v):
+    """Takes the array v as a parameter, and creates arrays for t and 
+    the resulting f(t,v). So for each i, t[i], y[i] will be an initial value,
+    an array of 100 times, and an array of 100 data points (y values).
+    This function returns a list l = [t, y]."""
+
     #creates an array of arrays, where each elemental array is a set of t's
     #corresponding to the set of y's for the SAME v_0
     t= np.array([np.linspace(0, float(i)/9.81, 100) for i in v])
@@ -28,7 +31,7 @@ def getVTY():
     #y[i]=vf(t[i], v)
     vf = np.vectorize(f)
     y = np.asarray([vf(t[i], v[i]) for i in range(len(v))])
-    return v, t, y
+    return t, y
 
 def plot1(v, t, y):
     """This function takes input in the form of v=array, t=array of arrays,
